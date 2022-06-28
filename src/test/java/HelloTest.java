5573ecc2acde0084a135acba67b75b9b11042ad6
AlymbekD <ArlanLee9@gmail.com>
2022-06-27 22:05:28 -0400

    started tracking .idea folder

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,14 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
+
+
+        // line 1
+
+        // line 2
+
+        //line 3
+
+
     }
 }
