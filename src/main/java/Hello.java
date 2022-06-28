4a142e9dc3f6c19f7f42d149d5971c97373a3378
AlymbekD <ArlanLee9@gmail.com>
2022-06-27 22:47:49 -0400

    Fixed AnotherOne

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
         System.out.println("Local Repo is already published to GitHUb");
+
     }
 }
